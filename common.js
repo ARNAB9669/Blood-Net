17b87a1bb3544e9cff6bf20b71e4266838da3e49
ARNAB9669 <arnabbiswas9669@gmail.com>
2025-07-25 00:02:18 +0530

    Add files via upload

---
+++ common.js
@@ -0,0 +1,9 @@
+document.querySelector('.blood > h1').addEventListener('click', () => {
+       document.querySelector('.groups').classList.toggle('on');
+   });
+
+   document.querySelector('.eye > h1').addEventListener('click', () => {
+       document.querySelector('.eyes').classList.toggle('on');
+   });
+
+// alert("Eye and other features are not implemented yet(Only blood)!");
